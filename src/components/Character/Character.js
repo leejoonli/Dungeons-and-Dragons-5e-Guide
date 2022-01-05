af14c2120bb7c26e21b521f99809af062d9e83dc
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-05 07:10:51 -0800

    create initial components

---
+++ src/components/Character/Character.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Character(props) {
+    return (
+        <div>
+            hello from character
+        </div>
+    );
+}
+
+export default Character;
---
+++ src/components/GameRulesAndMechanics/GameRulesAndMechanics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function GameRulesAndMechanics(props) {
+    return (
+        <div>
+            hello from game rules and mechanics
+        </div>
+    );
+}
+
+export default GameRulesAndMechanics;
