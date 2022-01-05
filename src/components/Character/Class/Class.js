69feec2bac6d305b7b8559aae002d79859ea7c69
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-05 13:41:36 -0800

    add more information to components with the api call

--- src/components/Character/Class/Class.js
+++ src/components/Character/Class/Class.js
@@ -15,7 +15,34 @@ function Class(props) {
 
     return (
         <div>
-            hello from class
+            {(dndClass === null ? null :
+                <>
+                    <h1>{dndClass.name}</h1>
+                    <h2>Class Features</h2>
+                    {/* <h3>Hit Points</h3>
+                    <ul>
+                        <li>Hit Dice:</li>
+                        <li>Hit Points at 1st Level:</li>
+                        <li>Hit Points at Higher Levels:</li>
+                    </ul> */}
+                    <h3>Proficiencies</h3>
+                    <ul>Weapons:
+                        {dndClass.proficiencies.map((element, index) => {
+                            return <li key={`${element.index}-${index}`}>{element.name}</li>;
+                        })}
+                    </ul>
+                    <ul>Saving Throws:
+                        {dndClass.saving_throws.map((element, index) => {
+                            return <li key={`${element.index}-${index}`}>{element.name}</li>
+                        })}
+                    </ul>
+                    <ul>Skills:
+                        {dndClass.proficiency_choices[0].from.map((element, index) => {
+                            return <li key={`${element.index}-${index}`}>{element.name}</li>
+                        })}
+                    </ul>
+                </>
+            )}
         </div>
     );
 }
