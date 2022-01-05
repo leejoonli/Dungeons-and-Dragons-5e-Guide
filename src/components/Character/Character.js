5037856f693b47b5679acd7f688aa183c850b880
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-05 08:40:38 -0800

    successfully map out first api call

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Character(props) {
+    // create state for classes, races, and stats
+    const [classes, setClasses] = useState(null);
+    const [races, setRaces] = useState(null);
+    const [stats, setStats] = useState(null);
+
+    useEffect(() => {
+        fetch("https://www.dnd5eapi.co/api/classes")
+            .then(res => res.json())
+            .then(res => setClasses(res.results))
+            .catch(console.error);
+    }, []);
+
     return (
         <div>
-            hello from character
+            Classes
+            <div>
+                {(classes === null) ? null : classes.map((element, index) => {
+                    return (
+                        <div key={`${element.index}-${index}`}>
+                            {element.name}
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     );
 }
