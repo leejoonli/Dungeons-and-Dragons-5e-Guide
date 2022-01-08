b9ac9c86994610ba1d5fc938d93de182220e2996
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-08 12:17:15 -0800

    add data from api to race component

--- src/components/Character/Race/Race.js
+++ src/components/Character/Race/Race.js
@@ -12,13 +12,27 @@ function Race(props) {
     useEffect(() => {
         fetch(`https://www.dnd5eapi.co/api/races/${id}`)
             .then(res => res.json())
-            .then(res => console.log(res))
+            .then(res => setRace(res))
             .catch(console.error);
     }, [id]);
-    
+
     return (
         <div>
-            hello from race
+            {race &&
+            <>
+                <h1>{race.name}</h1>
+                <h2>Age</h2>
+                <p>{race.age}</p>
+                <h2>Size</h2>
+                <p>{race.size}</p>
+                <h3>Size Description</h3>
+                <p>{race.size_description}</p>
+                <h2>Language</h2>
+                <p>{race.language_desc}</p>
+                <h2>Alignment</h2>
+                <p>{race.alignment}</p>
+            </>
+            }
         </div>
     );
 }
