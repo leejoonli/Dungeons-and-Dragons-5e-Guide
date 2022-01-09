82092db68ac4f6d6468ede1758af8e8adf27b378
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-09 10:46:06 -0800

    update spells component with different api

--- src/components/Character/Spells/Spells.js
+++ src/components/Character/Spells/Spells.js
@@ -1,20 +1,67 @@
 import React, { useState, useEffect } from 'react';
 
 function Spells(props) {
+    // create an array of objects to organize list of spells from api
+    const initState = [
+        {
+            spell_list: [],
+            level: 0,
+        },
+        {
+            spell_list: [],
+            level: 1,
+        },
+        {
+            spell_list: [],
+            level: 2,
+        },
+        {
+            spell_list: [],
+            level: 3,
+        },
+        {
+            spell_list: [],
+            level: 4,
+        },
+        {
+            spell_list: [],
+            level: 5,
+        },
+        {
+            spell_list: [],
+            level: 6,
+        },
+        {
+            spell_list: [],
+            level: 7,
+        },
+        {
+            spell_list: [],
+            level: 8,
+        },
+        {
+            spell_list: [],
+            level: 9,
+        }
+    ];
+
     // state to store list of skills from api
-    const [spells, setSpells] = useState(null);
+    const [spells, setSpells] = useState(initState);
 
     // useEffect to fetch list of skills from api
+    // using another api because the data list has more information without inputting more specific query parameters
     useEffect(() => {
-        fetch(`https://www.dnd5eapi.co/api/classes/druid/spells`)
+        fetch(`https://api.open5e.com/spells/?limit=321`)
             .then(res => res.json())
-            .then(res => setSpells(res))
+            .then(res => {
+                console.log(res.results.filter((element) => element.level === 'Cantrip'))
+            })
             .catch(console.error);
     }, []);
 
     return (
         <div>
-            {spells &&
+            {/* {spells &&
             <>
                 {spells.results.map((element, index) => {
                     return (
@@ -22,7 +69,7 @@ function Spells(props) {
                     )
                 })}
             </>
-            }
+            } */}
         </div>
     );
 }
