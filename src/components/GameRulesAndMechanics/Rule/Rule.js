f0edda0622512b316589ba56c60321919c301e72
jeremiahlee94 <jhwlee94@gmail.com>
2022-01-09 20:00:30 -0800

    work on rule component

--- src/components/GameRulesAndMechanics/Rule/Rule.js
+++ src/components/GameRulesAndMechanics/Rule/Rule.js
@@ -10,7 +10,7 @@ function Rule(props) {
         fetch(`https://www.dnd5eapi.co/api/rule-sections/${type}`)
             .then(res => res.json())
             .then(res => setRule(res.desc.split('#')))
-            .catch(console.error);
+            .catch(console.error)
     }, [type]);
 
     return (
@@ -20,7 +20,7 @@ function Rule(props) {
                 {rule.map((element, index) => {
                     return (
                         <div key={`${element.charAt(0)}-${index}`}>
-                            {element ? <p>{element.replace('\n', ':')}</p> : null}
+                            {element ? <p>{element.replace('\n', ': ')}</p> : null}
                         </div>
                     )
                 })}
